aa17b44ae0299d6bdf50c63db94a1f2dfc72ffd4
hong0980 <hong0980@qq.com>
2022-02-17 16:41:31 +0800

    update@2022-02-17-16-41

--- mk-slitaz64.sh
+++ mk-slitaz64.sh
@@ -4,11 +4,9 @@
 sed -i 's|iso.*boot|iso-old/boot|' dockerfile-rootfs*
 sed -i 's/slitaz/\$(date +%m%d-%H%M)-slitaz/' ./*/iso.sh
 sed -i '1i\https://mirrors.aliyun.com/slitaz' packages/mirrors.list
-cat <<-\EOF | tee -a packages/gen-slitaz* >/dev/null
-	wget -O $ROOTFS/bin/bpm git.io/bpm --no-check-certificate && chmod +x $ROOTFS/bin/bpm
-	wget -O $ROOTFS/bin/ansi git.io/ansi --no-check-certificate && chmod +x $ROOTFS/bin/ansi
-EOF
+sed -i '/profile/aecho "Asia/Shanghai" > $ROOTFS/etc/TZ\nwget -O $ROOTFS/bin/bpm git.io/bpm --no-check-certificate && chmod +x $ROOTFS/bin/bpm\nwget -O $ROOTFS/bin/ansi git.io/ansi --no-check-certificate && chmod +x $ROOTFS/bin/ansi' packages/gen-slitaz*
 wget -qO packages/updatetime.txt slitaz.cn/dl/rolling/slitaz-rolling-core64-chinese-updatetime.txt --no-check-certificate
+
 ps="dockerd sudocn-1.8.4 ghost-5.0.1 linux64-3.16.55 linux-3.16.55 yong-2.5.0 transmission-web-contrl-1.61 teasiu-5.0 xarchiver-cn-0.5.2 rar-6.0.0"
 for p in $ps; do
 	wget -nv -O packages/$p.tazpkg slitaz.cn/dl/$p.tazpkg --no-check-certificate || \
